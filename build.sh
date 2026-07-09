1c8a9174b9320c908de249974e3758d614d6619f
FLimeGreen <fynn.wegen@aol.com>
2026-07-09 19:07:22 +0200

    Auto sh Erkennung in build.

--- build.sh
+++ build.sh
@@ -5,14 +5,10 @@ echo "#!/bin/bash" >mytree.sh # Shebang einmal schreiben
 
 cat lib/config.sh >>mytree.sh # config datei als erstes laden
 
-cat lib/fzf_search.sh >>mytree.sh
-
-cat lib/dictionary_movement.sh >>mytree.sh
-cat lib/dictionary_order.sh >>mytree.sh
-cat lib/dictionary_draw.sh >>mytree.sh
-
-cat lib/run_command.sh >>mytree.sh
-cat lib/save_run_off_comands.sh >>mytree.sh
+for file in lib/*.sh; do
+  [[ "$file" == "lib/config.sh" ]] && continue
+  cat "$file" >>mytree.sh
+done
 
 cat Main.sh >>mytree.sh
 
