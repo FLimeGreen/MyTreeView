bd2d4d384581ade0d1d0496c1b8bb7b6c363749a
0kAj <122619846+0kAj@users.noreply.github.com>
2026-07-11 13:13:03 +0200

    adding uninstaller

--- build_installer.sh
+++ build_installer.sh
@@ -5,8 +5,13 @@ set -e
 echo "Checking MyTree build"
 
 PAYLOAD="out/mytree"
-TEMPLATE="installer/installer.template"
+INSTALLER_TEMPLATE="installer/installer.template"
+UNINSTALLER_TEMPLATE="installer/uninstaller.template"
+
 OUT_INSTALLER="out/installer.sh"
+OUT_UNINSTALLER="out/uninstaller.sh"
+
+mkdir -p out
 
 if [ ! -f "$PAYLOAD" ]; then
     echo "Missing build: $PAYLOAD"
@@ -16,21 +21,21 @@ fi
 
 echo "MyTree build found."
 
+
+# --------------------
+# Build installer
+# --------------------
+
 echo "Building installer"
 
-if [ ! -f "$TEMPLATE" ]; then
-    echo "Missing template: $TEMPLATE"
+if [ ! -f "$INSTALLER_TEMPLATE" ]; then
+    echo "Missing template: $INSTALLER_TEMPLATE"
     exit 1
 fi
 
-mkdir -p out
+printf '#!/bin/bash\n' > "$OUT_INSTALLER"
 
-# Installer Header + Template
-cat > "$OUT_INSTALLER" <<EOF
-#!/bin/bash
-EOF
-
-cat "$TEMPLATE" >> "$OUT_INSTALLER"
+cat "$INSTALLER_TEMPLATE" >> "$OUT_INSTALLER"
 
 # Payload anhängen
 cat "$PAYLOAD" >> "$OUT_INSTALLER"
@@ -39,3 +44,28 @@ chmod +x "$OUT_INSTALLER"
 
 echo "Installer built:"
 echo "  $OUT_INSTALLER"
+
+
+# --------------------
+# Build uninstaller
+# --------------------
+
+echo "Building uninstaller"
+
+if [ ! -f "$UNINSTALLER_TEMPLATE" ]; then
+    echo "Missing template: $UNINSTALLER_TEMPLATE"
+    exit 1
+fi
+
+printf '#!/bin/bash\n' > "$OUT_UNINSTALLER"
+
+cat "$UNINSTALLER_TEMPLATE" >> "$OUT_UNINSTALLER"
+
+chmod +x "$OUT_UNINSTALLER"
+
+echo "Uninstaller built:"
+echo "  $OUT_UNINSTALLER"
+
+
+echo
+echo "Build complete."
